d94c6b28b3eaf30a3515a009394583f026ef5aa3
Aulia Bismar <auliabismar@yahoo.com>
2024-03-30 07:13:38 +0700

    initial

---
+++ src/lib/components/Hero.svelte
@@ -0,0 +1,12 @@
+<section class="dark:bg-gray-100 dark:text-gray-800">
+	<div class="container mx-auto flex flex-col items-center px-4 py-16 text-center md:py-32 md:px-10 lg:px-32 xl:max-w-3xl">
+		<h1 class="text-4xl font-bold leading-none sm:text-5xl">Quisquam necessita vel
+			<span class="dark:text-orange-600">laborum doloribus</span>delectus
+		</h1>
+		<p class="px-8 mt-8 mb-12 text-lg">Cupiditate minima voluptate temporibus quia? Architecto beatae esse ab amet vero eaque explicabo!</p>
+		<div class="flex flex-wrap justify-center">
+			<button class="px-8 py-3 m-2 text-lg font-semibold rounded dark:bg-orange-600 dark:text-gray-50">Get started</button>
+			<button class="px-8 py-3 m-2 text-lg border rounded dark:text-gray-900 dark:border-gray-300">Learn more</button>
+		</div>
+	</div>
+</section>
